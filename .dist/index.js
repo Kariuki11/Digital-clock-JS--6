5ed995c5ddf1212edf2d496b0c462daebc413414
Kariuki11 <irungukenneth433@gmail.com>
2024-06-04 14:50:35 +0300

    Last commits

--- .dist/index.js
+++ .dist/index.js
@@ -2,11 +2,15 @@
 function updateClock(){
 
     const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
-    const timeString = `${hours}:${minutes}:${seconds}`;
+    let hours = now.getHours();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, 0)
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
     document.getElementById("clock").textContent = timeString;
 }
 
 updateClock();
+setInterval(updateClock, 1000);
