49707d8030d4eb55c43c2e690ed7554cefc2a366
Kariuki11 <irungukenneth433@gmail.com>
2024-06-04 14:33:08 +0300

    New changes

--- .dist/index.js
+++ .dist/index.js
@@ -2,4 +2,11 @@
 function updateClock(){
 
     const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById("clock").textContent = timeString;
 }
+
+updateClock();
